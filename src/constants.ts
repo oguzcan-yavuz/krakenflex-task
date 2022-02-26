89b6a40e686df1896cf2db39d5d2ac9c2666d9ba
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2022-02-26 17:16:57 +0300

    implement list outages endpoint

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const OUTAGE_API_BASE_URL =
+  'https://api.krakenflex.systems/interview-tests-mock-api/v1/';
--- src/outage.service.ts
+++ src/outage.service.ts
@@ -1,8 +1,21 @@
 import { Outage, OutageWithDeviceNames, SiteInfo } from './types';
+import axios, { AxiosInstance } from 'axios';
+import { OUTAGE_API_BASE_URL } from './constants';
 
 export class OutageService {
+  private httpClient: AxiosInstance;
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: OUTAGE_API_BASE_URL,
+      timeout: 1000,
+      // headers: {'X-Custom-Header': 'foobar'} // TODO: add the api-key here
+    });
+  }
+
   async list(): Promise<Outage[] | undefined> {
-    return [];
+    const { data: outages } = await this.httpClient.get('/outages');
+
+    return outages;
   }
 
   async getSiteInfo(siteName: string): Promise<SiteInfo | undefined> {
