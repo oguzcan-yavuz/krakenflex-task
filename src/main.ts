21ba6ae79a23264017db04c41bc1bda7531833b3
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2022-02-26 13:52:33 +0300

    add filtering for outages based on start date

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,27 @@ import { Outage, SiteInfo } from './types';
 import { OutageService } from './outage.service';
 import { NotFoundError } from './errors/not-found-error';
 
+interface GetOutageOptions {
+  after: Date;
+}
+
 export class Main {
   constructor(private readonly outageService: OutageService) {}
 
-  async getOutages(): Promise<Outage[]> {
-    return this.outageService.list();
+  async getOutages(options?: GetOutageOptions): Promise<Outage[]> {
+    const outages = await this.outageService.list();
+
+    if (!options) {
+      return outages;
+    }
+
+    if (options.after) {
+      const outagesAfterGivenDate = outages.filter(
+        (outage) => new Date(outage.begin) > options.after,
+      );
+
+      return outagesAfterGivenDate;
+    }
   }
 
   async getSiteInfo(name: string): Promise<SiteInfo> {
--- test/unit/main.spec.ts
+++ test/unit/main.spec.ts
@@ -80,6 +80,40 @@ describe('main', () => {
       // Assert
       expect(outages).toEqual(mockOutages);
     });
+
+    it('should filter out the outages started before the given date', async () => {
+      // Arrange
+      const after = new Date('2022-01-01T00:00:00.000Z');
+      const outagesAfterGivenDate = [
+        {
+          id: '002b28fc-283c-47ec-9af2-ea287336dc1b',
+          begin: '2022-05-23T12:21:27.377Z',
+          end: '2022-11-13T02:16:38.905Z',
+        },
+        {
+          id: '002b28fc-283c-47ec-9af2-ea287336dc1b',
+          begin: '2022-12-04T09:59:33.628Z',
+          end: '2022-12-12T22:35:13.815Z',
+        },
+        {
+          id: '04ccad00-eb8d-4045-8994-b569cb4b64c1',
+          begin: '2022-07-12T16:31:47.254Z',
+          end: '2022-10-13T04:05:10.044Z',
+        },
+        {
+          id: '086b0d53-b311-4441-aaf3-935646f03d4d',
+          begin: '2022-07-12T16:31:47.254Z',
+          end: '2022-10-13T04:05:10.044Z',
+        },
+      ];
+      when(mockOutageService.list()).thenResolve(mockOutages);
+
+      // Act
+      const outages = await main.getOutages({ after });
+
+      // Assert
+      expect(outages).toEqual(outagesAfterGivenDate);
+    });
   });
 
   describe('getSiteInfo()', () => {
