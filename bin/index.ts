a0d7b1022558786010c8a7e17e7bce1dacb4f85f
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2022-02-27 01:19:55 +0300

    add input validation for date

--- bin/index.ts
+++ bin/index.ts
@@ -1,46 +1,57 @@
 #!/usr/bin/env node
 import 'dotenv/config';
-import { Command } from 'commander';
+import { Command, InvalidArgumentError } from 'commander';
+import { isISO8601 } from 'validator';
 import { Main } from '../src/main';
 import { OutageService } from '../src/outage.service';
 
 type Input = {
   apiKey: string;
   siteId: string;
-  dateAfter: string;
+  dateAfter: Date;
 };
 
-const program = new Command();
+const parseDate = (value: string): Date => {
+  const isValid = isISO8601(value);
+  if (!isValid) {
+    throw new InvalidArgumentError('Date must be provided in ISO-8601 format.');
+  }
+  const date = new Date(value);
 
-program.name('create-outages').version('1.0.0');
-program
-  .option(
-    '-a, --api-key <key>',
-    'api key for the outage service',
-    process.env.OUTAGE_API_KEY,
-  )
-  .option(
-    '-s, --site-id <siteId>',
-    'site id to add outages',
-    'norwich-pear-tree',
-  )
-  .option(
-    '-d, --date-after <date>',
-    'beginning date of the outages',
-    '2022-01-01T00:00:00.000Z',
-  );
-
-program.parse();
-
-const { apiKey, siteId, dateAfter } = program.opts<Input>();
-
-const date = new Date(dateAfter);
-const outageService = new OutageService(apiKey);
-const main = new Main(outageService);
+  return date;
+};
 
 (async () => {
   try {
-    await main.run(siteId, date);
+    const program = new Command();
+
+    program.name('create-outages').version('1.0.0');
+    program
+      .option(
+        '-a, --api-key <key>',
+        'api key for the outage service',
+        process.env.OUTAGE_API_KEY,
+      )
+      .option(
+        '-s, --site-id <siteId>',
+        'site id to add outages',
+        'norwich-pear-tree',
+      )
+      .option<Date>(
+        '-d, --date-after <date>',
+        'starting date of the outages in ISO-8601 format',
+        parseDate,
+        new Date('2022-01-01T00:00:00.000Z'),
+      );
+
+    program.parse();
+
+    const { apiKey, siteId, dateAfter } = program.opts<Input>();
+
+    const outageService = new OutageService(apiKey);
+    const main = new Main(outageService);
+
+    await main.run(siteId, dateAfter);
     console.log('Outages created successfully!');
   } catch (err) {
     console.error(err);
